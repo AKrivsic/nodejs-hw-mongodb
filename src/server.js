252dc5781729abe254ae5977c84fc1f4ab42d265
Alexandr Krivsic <o.kryvshych@gmail.com>
2025-09-08 13:27:31 +0200

    server.js errorHandler, notFoundHandler

--- src/server.js
+++ src/server.js
@@ -6,6 +6,8 @@ import cookieParser from 'cookie-parser';
 import { getEnvVar } from './utils/getEnvVar.js';
 import contactsRouter from './routers/contacts.js';
 import authRouter from './routers/auth.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
+import { errorHandler } from './middlewares/errorHandler.js';
 
 export function setupServer() {
   const app = express();
@@ -22,6 +24,9 @@ export function setupServer() {
   app.use('/auth', authRouter);
   app.use('/contacts', contactsRouter);
 
+  app.use(notFoundHandler);
+
+  app.use(errorHandler);
 
   const PORT = Number(getEnvVar('PORT', '3000'));
   app.listen(PORT, () => {
